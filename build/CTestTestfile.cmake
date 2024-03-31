e2cf5c659416bb3ec4b383586ec84c8ce2c3108f
2ueq <xx040504@gmail.com>
2024-03-31 11:29:29 +0800

    update ignored files

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/coups/CP2024
-# Build directory: /home/coups/CP2024/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UnitTest "UnitTests_catch2")
-set_tests_properties(UnitTest PROPERTIES  _BACKTRACE_TRIPLES "/home/coups/CP2024/CMakeLists.txt;21;add_test;/home/coups/CP2024/CMakeLists.txt;0;")
-subdirs("_deps/catch2-build")
